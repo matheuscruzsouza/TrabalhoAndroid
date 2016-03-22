c3c3405b10e6a0e8a3a42475cc8289c5efe1fe21
“matheuscruzsouza” <matheuscruzsouza@gmail.com>
2016-03-22 20:23:39 -0300

    Lista de Disciplinas

--- app/src/main/java/com/example/matheus/appandroid/AddDisciplinaAluno_view.java
+++ app/src/main/java/com/example/matheus/appandroid/AddDisciplinaAluno_view.java
@@ -6,31 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
 
-public class Aluno_view extends AppCompatActivity {
+public class AddDisciplinaAluno_view extends AppCompatActivity {
 
     private String matricula;
     private ListView LV_Disciplinas;
     private DatabaseAdapter BD;
-    private Button BT_AddDisc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aluno);
+        setContentView(R.layout.activity_add_disciplina_aluno);
 
         Bundle args = getIntent().getExtras();
         matricula = args.getString("matricula");
 
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Aplica a setinha no actionBar
-
         BD = new DatabaseAdapter(this);
 
-        final List<String> disciplinas = BD.BuscarDisciplinasAluno(matricula) ;
+        final List<String> disciplinas = BD.BuscarDisciplinas() ;
 
         ArrayAdapter<String> itemsAdapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, disciplinas);
@@ -40,29 +36,13 @@ public class Aluno_view extends AppCompatActivity {
         LV_Disciplinas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent tela = new Intent(Aluno_view.this, Disciplina_view.class);
-                Bundle materia = new Bundle();
-                materia.putString("materia_nome", disciplinas.get(position));
-                tela.putExtras(materia);
-                startActivity(tela);
-            }
-        });
-
-        BT_AddDisc = (Button) findViewById(R.id.BT_AddDisc);
-
-        BT_AddDisc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent tela = new Intent(Aluno_view.this, AddDisciplinaAluno_view.class);
+                BD.adicionarDisciplina(matricula, disciplinas.get(position));
+                Intent tela = new Intent(AddDisciplinaAluno_view.this, Aluno_view.class);
                 Bundle materia = new Bundle();
                 materia.putString("matricula", matricula);
                 tela.putExtras(materia);
                 startActivity(tela);
             }
         });
-
-
-
     }
 }
-
